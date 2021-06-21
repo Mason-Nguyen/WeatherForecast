0beabeb15ac9ff2ae515788821208ab3bf8977d0
Hau Nguyen <quanghau05@gmail.com>
2021-06-21 22:14:57 +0700

    Modify style for CurrentWeather component

--- TLY.WeatherForecast_UI/src/scripts/components/CurrentWeather.js
+++ TLY.WeatherForecast_UI/src/scripts/components/CurrentWeather.js
@@ -2,24 +2,19 @@ import React from "react"
 import config from '../../config/config.json'
 import '../../scss/CurrentWeather.scss'
 
-const CurrentWeather = ({currentData}) => 
+const CurrentWeather = ({ currentData }) =>
     <div className='current-weather flex-center-col'>
+        <label id='label-temperature'>{currentData.temp}</label>
         <label className='bold-label city-label block-item'>{currentData.cityName}</label>
-        <label className='current-time text-center'>{currentData.currentTime}</label>
-
-        <div className='weather-description flex-center'>
-            <img src={`${config.Icon_Url}${currentData.icon}.png`} alt='no content' className='weather-icon'/>
-            <label id='label-temperature'>{currentData.temp}</label>
-        </div>
         <label className='label-description text-center'>{currentData.description}</label>
-
+        <div className='weather-description flex-center'>
+            <img src={`${config.Icon_Url}${currentData.icon}@2x.png`} alt='no content' className='weather-icon' />
+        </div>
         <div className='flex-center'>
             <div className='flex-center-item'>
-                <label className='block-item'>Humidity</label>
                 <label className='humidity bold-label'>{currentData.humidity}</label>
             </div>
             <div className='flex-center-item'>
-                <label className='block-item'>Wind Speed</label>
                 <label className='wind-speed bold-label'>{currentData.windSpeed}</label>
             </div>
         </div>
